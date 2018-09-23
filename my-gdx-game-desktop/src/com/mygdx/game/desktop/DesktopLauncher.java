8335fdce7976f4087e3e30ff0f13a90dfd65cf21
Harry Stevens <Harry Stevens@DESKTOP-56C2BK3.home>
2018-09-23 18:22:26 +0100

    First git commit

--- my-gdx-game-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ my-gdx-game-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.foregroundFPS = 60;
+		config.foregroundFPS = 50;
 		new LwjglApplication(new MyGdxGame(), config);	
 	}
 }
