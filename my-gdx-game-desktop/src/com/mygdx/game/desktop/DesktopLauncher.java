79f014ac7c2838489709cf16a8c67dd4b9cd7ae2
Harry Stevens <Harry Stevens@DESKTOP-56C2BK3.home>
2018-09-23 18:38:57 +0100

    second

--- my-gdx-game-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ my-gdx-game-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.foregroundFPS = 50;
+		config.foregroundFPS = 60;
 		new LwjglApplication(new MyGdxGame(), config);	
 	}
 }
